a2fb0e458a5703ebd94eb38ccb84cfb729d82672
Jtmcflea <Jtmcflea@gmail.com>
2026-07-27 15:19:04 -0700

    add entering lobby from main menu

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuButtons : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenLobby()
+    {
+        SceneManager.LoadScene("LobbyScene_v1");
+    }
+
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
