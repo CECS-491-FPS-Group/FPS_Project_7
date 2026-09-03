ea0f1d90d8f000a345fbb6f72a75a457d355056e
Jtmcflea <Jtmcflea@gmail.com>
2026-09-03 16:49:37 -0700

    make game state length timers public

--- Assets/Scripts/RoundHandler/RoundTimer.cs
+++ Assets/Scripts/RoundHandler/RoundTimer.cs
@@ -9,27 +9,31 @@ using UnityEngine.SceneManagement;
 
 public class RoundTimer : MonoBehaviour
 {
+    public int preGameLength;
+    public int gameLength;
+    public int postGameLength;
     // 20s pregame, 360s (5m) game, 10s postgame
+    [HideInInspector]
     public enum gameStates
     {
         PREGAME = 0,
         GAME = 1,
         POSTGAME = 2
     }
-    
+    [HideInInspector]
     public bool timerIsRunning;
-    float timeRemaining;
+    [HideInInspector]
     public string timeText;
+    [HideInInspector]
     public gameStates currentState;
+    [HideInInspector]
     public string currentStateName;
+    float timeRemaining;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        timerIsRunning = true;
-        // start pregame when scene is loaded
-        currentState = gameStates.PREGAME;
-        currentStateName = "Pregame";
-        timeRemaining = 20; 
+        startGame();
     }
 
     // Update is called once per frame
@@ -44,29 +48,18 @@ public class RoundTimer : MonoBehaviour
             }
             else
             {
-                // if in pregame, switch to game
-                if (currentState == gameStates.PREGAME)
-                {
-                    timeRemaining = 5;
-                    currentState = gameStates.GAME;
-                    currentStateName = "Game";
-                }
-
-                // if in game, switch to postgame
-                else if (currentState == gameStates.GAME)
-                {
-                    timeRemaining = 10;
-                    currentState = gameStates.POSTGAME;
-                    currentStateName = "Postgame";
-                }
-
-                // if in postgame, reload scene
-                else if (currentState == gameStates.POSTGAME)
-                {
-                    SceneManager.LoadScene("RoundImplementation");
-                }
+                updateGameState();
             }
         }
+    }
+
+    void startGame()
+    {
+        timerIsRunning = true;
+        // start pregame when scene is loaded
+        currentState = gameStates.PREGAME;
+        currentStateName = "Pregame";
+        timeRemaining = preGameLength; 
     }
 
     // just doing a bit of floating point math every frame B) very cool and efficient
@@ -80,5 +73,30 @@ public class RoundTimer : MonoBehaviour
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
 
         timeText = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void updateGameState()
+    {
+        switch (currentState)
+        {
+            // if we're in the pregame, switch to main game
+            case gameStates.PREGAME:
+                timeRemaining = gameLength;
+                currentState = gameStates.GAME;
+                currentStateName = "Game";
+                break;
+            
+            // if we're in the main game, switch to postgame
+            case gameStates.GAME:
+                timeRemaining = postGameLength;
+                currentState = gameStates.POSTGAME;
+                currentStateName = "Postgame";
+                break;
+
+            // if we're in the postgame, reload the scene
+            case gameStates.POSTGAME:
+                SceneManager.LoadScene("RoundImplementation");
+                break;
+        }
     }
 }
